fe7c29b5c8a144ae33e09c4f4f7244bd9100cac8
Kunal Hinduja <48852726+kunalhindujaa@users.noreply.github.com>
2020-02-10 12:51:17 +0530

    Old Detail component added again

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Detail(props) {
-  return (
-    <div className="detail">
-      <p> Name : {props.userName}</p>
-      <p> Email :{props.emailId}</p>
-      <p> Website :{props.website}</p>
-    </div>
-  );
+function Detail(props){
+    return (
+        <div className="detail">
+            <p> Name : {props.userName}</p>
+            <p> Email :{props.emailId}</p>
+            <p> Website :{props.website}</p>
+        </div>
+    );
 }
 
 export default Detail;
