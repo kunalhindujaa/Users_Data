125af16719ede2538839a4cb30d0b5ccb32062f4
Kunal Hinduja <48852726+kunalhindujaa@users.noreply.github.com>
2020-02-26 16:26:46 +0530

    DisplayDetails made functional component

--- src/components/DisplayDetails.jsx
+++ src/components/DisplayDetails.jsx
@@ -1,34 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 
-class DisplayDetails extends Component {
-  state = {
-    name: this.props.location.state.name,
-    phone: this.props.location.state.phone,
-    listItem: this.props.location.state.listItem,
-    gender: this.props.location.state.gender,
-    registered: this.props.location.state.registered
-  };
-  render() {
-    return (
-      <div>
-        <h1 className="header"> Registration Details </h1>
-        <div class="register-details">
-          <p> Name : {this.state.name}</p>
-          <br />
-          <p> Contact Number : {this.state.phone}</p>
-          <br />
-          <p> Gender : {this.state.gender}</p>
-          <br />
-          <p>
-            {" "}
-            Previously registered for the course : {this.state.registered}{" "}
-          </p>
-          <br />
-          <p> Course : {this.state.listItem}</p>
-        </div>
+const DisplayDetails = props => {
+  return (
+    <div>
+      <h1 className="header"> Registration Details </h1>
+      <div class="register-details">
+        <p> Name : {props.location.state.name}</p>
+        <br />
+        <p> Contact Number : {props.location.state.phone}</p>
+        <br />
+        <p> Gender : {props.location.state.gender}</p>
+        <br />
+        <p>
+          {" "}
+          Previously registered for the course :{" "}
+          {props.location.state.registered}{" "}
+        </p>
+        <br />
+        <p> Course : {props.location.state.listItem}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default DisplayDetails;
